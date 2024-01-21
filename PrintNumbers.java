be305c5e287fa64218b375cb7079fcb682da9f77
Abinithya <154597389+abinithya-ginnu@users.noreply.github.com>
2024-01-21 18:47:54 +0530

    Create PrintNumbers.java

---
+++ PrintNumbers.java
@@ -0,0 +1,17 @@
+
+class Solution
+{
+    
+  public void printNos(int N)
+    {
+        //Your code here
+        String str = "";
+        int i = 0;
+        while (i < N){
+            i = i + 1;
+            str += i + " ";
+        }
+        System.out.print(str);
+    }
+}
+
