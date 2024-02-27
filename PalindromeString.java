4f0d6431b42e19c367d0433370c09dd9d690472c
Abinithya <154597389+abinithya-ginnu@users.noreply.github.com>
2024-02-27 19:16:27 +0530

    Create PalindromeString.java

---
+++ PalindromeString.java
@@ -0,0 +1,36 @@
+/*
+Given a string S, check if it is palindrome or not.
+
+Example 1:
+
+Input: S = "abba"
+Output: 1
+Explanation: S is a palindrome
+Example 2:
+
+Input: S = "abc" 
+Output: 0
+Explanation: S is not a palindrome
+Your Task:
+You don't need to read input or print anything. Complete the function isPalindrome()which accepts string S and returns an integer value 1 or 0.
+
+Expected Time Complexity: O(Length of S)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 <= Length of S<= 2*105
+*/
+
+//Solution:
+
+class Solution {
+    int isPalindrome(String S) {
+        int length = S.length();
+        for(int i = (length/2)-1; i >= 0; i--){
+            if(S.charAt(i) != S.charAt(length-1-i)){
+                return 0; 
+            }
+        }
+        return 1;
+    }
+};
